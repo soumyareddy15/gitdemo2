52f6d2b6b2fe334ec1d74c92e8536e9ecea64268
soumyareddy15 <71117823+soumyareddy15@users.noreply.github.com>
2021-07-28 09:44:30 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Prjthirdapplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello alll!");
         }
     }
 }
